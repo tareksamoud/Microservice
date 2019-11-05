2ad93ba86e06a4403a717395d46dd0d8c0f3918b
Tarek Samoud <ts@sasse-mertins.de>
2019-11-05 22:50:55 +0100

    create produit microservice project

---
+++ Produit-Service/src/main/java/com/sid/ProduitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProduitServiceApplication.class, args);
+    }
+
+}
